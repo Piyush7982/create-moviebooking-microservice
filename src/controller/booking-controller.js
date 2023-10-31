954a7e10fef105f22290506bec474a11939a5eae
Piyush7982 <ajain9213742679@gmail.com>
2023-10-31 23:33:55 +0530

    minor fixes

--- src/controller/booking-controller.js
+++ src/controller/booking-controller.js
@@ -1,87 +1,93 @@
-const {BookingService}= require("../services")
-const {SuccessResponse,ErrorResponse}= require("../util/common")
-const {TokenUtil}= require("../util/common")
+const { BookingService } = require("../services");
+const { SuccessResponse, ErrorResponse } = require("../util/common");
+const { TokenUtil } = require("../util/common");
 
+async function bookingCreate(req, res) {
+  try {
+    console.log("inside booking");
+    const uid = await TokenUtil.decodeToken(req.cookies.access_token);
 
+    const response = await BookingService.createBooking({
+      userId: uid.id,
+      showId: req.body.showId,
+      totalTickets: req.body.totalTickets,
+    });
+    SuccessResponse.Data = response;
+    SuccessResponse.Message = "succesfully created";
+    return res.json(SuccessResponse);
+  } catch (error) {
+    ErrorResponse.Error = error;
+    res
+      .status(
+        error.statusCode ? error.statusCode : StatusCodes.INTERNAL_SERVER_ERROR
+      )
+      .json(ErrorResponse);
 
-async function bookingCreate(req,res){
-    try {
-        console.log("inside booking")
-        const uid= await TokenUtil.decodeToken(req.cookies.access_token)
-        
-        const response= await BookingService.createBooking({userId:uid.id,showId:req.body.showId,totalTickets:req.body.totalTickets})
-        SuccessResponse.Data=response
-        SuccessResponse.Message="succesfully created"
-        return res.json(SuccessResponse)
-        
-    } catch (error) {
-        
-        ErrorResponse.Error=error
-        res
-        // .status(error.statusCode)
-        .json(ErrorResponse)
-        
-        throw error
-    }
+    throw error;
+  }
 }
-async function bookingFindAll(req,res){
-    
-    try {
-        const response= await BookingService.findAllBooking()
-        SuccessResponse.Data=response
-        SuccessResponse.Message="succesfully created"
-        return res.json(SuccessResponse)
-        
-    } catch (error) {
-        
-        ErrorResponse.Error=error
-        res
-        // .status(error.statusCode)
-        .json(ErrorResponse)
-        
-        throw error
-    }
-}
+async function bookingFindAll(req, res) {
+  try {
+    const response = await BookingService.findAllBooking();
+    SuccessResponse.Data = response;
+    SuccessResponse.Message = "succesfully created";
+    return res.json(SuccessResponse);
+  } catch (error) {
+    ErrorResponse.Error = error;
+    res
+      .status(
+        error.statusCode ? error.statusCode : StatusCodes.INTERNAL_SERVER_ERROR
+      )
+      .json(ErrorResponse);
 
-
-
-async function bookingUpdatePayment(req,res){
-    try {
-        const status= req.body.status
-        const response= await BookingService.isPaymentSuccesfull({id:req.body.bookingId},status)
-        SuccessResponse.Data=response
-        SuccessResponse.Message="succesfully created"
-        return res.json(SuccessResponse)
-        
-    } catch (error) {
-        
-        ErrorResponse.Error=error
-        res
-        // .status(error.statusCode)
-        .json(ErrorResponse)
-        
-        throw error
-    }
-}
-async function bookingDelete(req,res){
-    try {
-        const bookingId=req.body.bookingId
-        const response= await BookingService.deleteBooking(bookingId)
-        SuccessResponse.Data=response
-        SuccessResponse.Message="succesfully created"
-        return res.json(SuccessResponse)
-        
-    } catch (error) {
-        
-        ErrorResponse.Error=error
-        res
-        // .status(error.statusCode)
-        .json(ErrorResponse)
-        
-        throw error
-    }
+    throw error;
+  }
 }
 
-const bookingController={bookingCreate,bookingDelete,bookingUpdatePayment,bookingFindAll}
-module.exports=bookingController
-    
+async function bookingUpdatePayment(req, res) {
+  try {
+    const status = req.body.status;
+    const response = await BookingService.isPaymentSuccesfull(
+      { id: req.body.bookingId },
+      status
+    );
+    SuccessResponse.Data = response;
+    SuccessResponse.Message = "succesfully created";
+    return res.json(SuccessResponse);
+  } catch (error) {
+    ErrorResponse.Error = error;
+    res
+      .status(
+        error.statusCode ? error.statusCode : StatusCodes.INTERNAL_SERVER_ERROR
+      )
+      .json(ErrorResponse);
+
+    throw error;
+  }
+}
+async function bookingDelete(req, res) {
+  try {
+    const bookingId = req.body.bookingId;
+    const response = await BookingService.deleteBooking(bookingId);
+    SuccessResponse.Data = response;
+    SuccessResponse.Message = "succesfully created";
+    return res.json(SuccessResponse);
+  } catch (error) {
+    ErrorResponse.Error = error;
+    res
+      .status(
+        error.statusCode ? error.statusCode : StatusCodes.INTERNAL_SERVER_ERROR
+      )
+      .json(ErrorResponse);
+
+    throw error;
+  }
+}
+
+const bookingController = {
+  bookingCreate,
+  bookingDelete,
+  bookingUpdatePayment,
+  bookingFindAll,
+};
+module.exports = bookingController;
--- src/index.js
+++ src/index.js
@@ -1,34 +1,19 @@
-const express= require("express")
-const cookieParser = require('cookie-parser')
-const {router}= require("./routes")
-const {ServerPort}= require("./config")
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const { router } = require("./routes");
+const { ServerPort } = require("./config");
 
-
-const app= express()
+const app = express();
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(cookieParser())
-app.use("/api",router)
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use("/api", router);
 
-
-app.listen(ServerPort,()=>{
-    console.log(`Server Started on Port ${ServerPort}`)
-})
+app.listen(ServerPort, () => {
+  console.log(`Server Started on Port ${ServerPort}`);
+});
 // async function testing(){
 //     const response= await Booking.findByPk(1)
 //     console.log(response.dataValues.totalTickets)
 // }
 // testing()
-
-
-
-
-
-
-
-
-
-
-
-
-
